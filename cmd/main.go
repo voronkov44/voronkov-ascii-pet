8a16f9a5e1eddfbba9fb22606809440931dc19d4
voronkov44 <andrewfilzz0@gmail.com>
2025-05-02 00:02:31 +0300

    add logfile

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"voronkov-ascii-pet/internal/pet"
 )
 
 func main() {
+	logFile, err := os.OpenFile("ascii-pet.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file:", err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
 	router := http.NewServeMux()
 	pet.NewPetHandler(router)
 
@@ -16,5 +25,7 @@ func main() {
 	}
 
 	log.Println("Server started at port 8080")
-	server.ListenAndServe()
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalln("Failed to start server:", err)
+	}
 }
--- internal/pet/model.go
+++ internal/pet/model.go
@@ -4,7 +4,3 @@ type Pet struct {
 	Ascii       string `json:"ascii"`
 	Description string `json:"description"`
 }
-
-type ErrorResponse struct {
-	Error string `json:"error"`
-}
